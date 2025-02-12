3990f2e83f258083442530547bc5cf4856dc0a5a
Ryan Mus <153521842+Ryan-Mus@users.noreply.github.com>
2025-02-12 10:59:28 +0100

    Initial Commit

---
+++ VulkanProject/VulkanProject.cpp
@@ -0,0 +1,12 @@
+﻿// VulkanProject.cpp : Defines the entry point for the application.
+//
+
+#include "VulkanProject.h"
+
+using namespace std;
+
+int main()
+{
+	cout << "Hello CMake." << endl;
+	return 0;
+}
